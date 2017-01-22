533e26782b3433b2c7b62c0e3b29308dfd6a2652
Kenji Rikitake <kenji.rikitake@acm.org>
2017-01-22 11:33:37 +0900

    Revised config/config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,28 +1,24 @@
 use Mix.Config
 
+# For configuring IP address and TCP port number,
+# use the parameters for :poudriere_elixir_web
+
 config :poudriere_elixir_web,
   ip: {127, 0, 0, 1},
   port: 8080
 
-import_config "#{Mix.env}.exs"
+# DO NOT REMOVE THIS:
+# This is a mandatory configuration of MIME types
+# for MIME module
 
-# You can configure for your application as:
-#
-#     config :poudriere_elixir_web, key: :value
-#
-# And access this configuration in your application as:
-#
-#     Application.get_env(:poudriere_elixir_web, :key)
-#
-# Or configure a 3rd-party app:
-#
-#     config :logger, level: :info
-#
+config :mime, :types, %{
+  "text/mathml" => ["mml"]
+}
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
 # Configuration from the imported file will override the ones defined
 # here (which is why it is important to import them last).
-#
-#     import_config "#{Mix.env}.exs"
+
+import_config "#{Mix.env}.exs"
