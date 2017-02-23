e622f34dff0a2a18649bf8dc28eea82c84c94260
Kenji Rikitake <kenji.rikitake@acm.org>
2017-02-23 20:30:05 +0900

    config.exs: remove unnecessary MIME type

--- config/config.exs
+++ config/config.exs
@@ -7,14 +7,6 @@ config :poudriere_elixir_web,
   ip: {127, 0, 0, 1},
   port: 8080
 
-# DO NOT REMOVE THIS:
-# This is a mandatory configuration of MIME types
-# for MIME module
-
-config :mime, :types, %{
-  "text/mathml" => ["mml"]
-}
-
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
