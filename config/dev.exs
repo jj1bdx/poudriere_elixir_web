e186e4ae485efba0b02126d193e8e371d7b5b7ef
Kenji Rikitake <kenji.rikitake@acm.org>
2017-04-06 12:07:21 +0900

    Add config/dev.exs (skeleton)

---
+++ config/dev.exs
@@ -0,0 +1,8 @@
+use Mix.Config
+
+# For configuring IP address and TCP port number,
+# use the parameters for :poudriere_elixir_web
+
+config :poudriere_elixir_web,
+  ip: {127, 0, 0, 1},
+  port: 8080
